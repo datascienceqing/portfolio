b9bc78a4ee4f62eb8c5695fd5d26baa3fbc90ef6
datascienceqing <qingzhou.cn@gmail.com>
2021-02-14 18:31:02 +0000

    change job

--- static/js/home.js
+++ static/js/home.js
@@ -2,10 +2,10 @@ $(function() {
   $(".typed").typed({
     strings: [
       "Qing Zhou<br/>" + 
-      "><span class='caret'>$</span> work: Data warehouse, Statistic Analysis, Cloud Data Engineering, NLP <br/> ^100" +
+      "><span class='caret'>$</span> work: Cloud Data Engineering, Machine Learning, Business Intelligence Analysis <br/> ^100" +
       "><span class='caret'>$</span> hobbies: skiing, biking, and camping <br/> ^300" +
       "><span class='caret'>$</span> highlight: <a href='https://github.com/datascienceqing/HeartSound'>Heart Sound Analysis</a>,  <a href='https://github.com/datascienceqing/SparkETL_dbfs/blob/master/songDatabase.ipynb'>ETL in Azure Databricks</a>, <a href='https://youtu.be/5R_c7XdWmZg'>race car simulation</a><br/>" +
-      "><span class='caret'>$</span> job:Data Integration Specialist at Kaiser Permanente Center for Health Research <br/> ^100"     ],
+      "><span class='caret'>$</span> job:Hudson Data/Machine Learning Engineer  <br/> ^100"     ],
     showCursor: true,
     cursorChar: '_',
     autoInsertCss: true,
